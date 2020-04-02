c37d5e9109a4974a413dc695da2ee12c0e016c6b
Teun van Laarhoven <teunvanlaarhoven@gmail.com>
2020-04-02 17:10:06 +0200

    Added blank Projects for services

---
+++ service-tweets/src/main/java/com/kwetter/servicetweets/ServiceTweetsApplication.java
@@ -0,0 +1,13 @@
+package com.kwetter.servicetweets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTweetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTweetsApplication.class, args);
+	}
+
+}
---
+++ service-tweets/src/test/java/com/kwetter/servicetweets/ServiceTweetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwetter.servicetweets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTweetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
