fc543ca6edb1cfbb5158e710059ef403844ea387
Teun van Laarhoven <teunvanlaarhoven@gmail.com>
2020-06-26 17:42:29 +0200

    adjusted IP's in frontend to be external IP's instead of relying on DNS

--- angular-frontend/src/environments/environment.prod.ts
+++ angular-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  tweetUrl: 'http://tweets:8081',
-  userUrl: 'http://users:8082',
-  authUrl: 'http://auth:8083'
+  tweetUrl: 'http://10.0.198.18:8081',
+  userUrl: 'http://10.0.206.119:8082',
+  authUrl: 'http://10.0.129.79:8083'
 };
